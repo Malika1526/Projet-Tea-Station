201b00b8dc8fcee0e19365e7114c78413278f5a8
AdnamDemaurisa59 <adnam.kouddemaurisa@outlook.fr>
2024-10-16 22:57:09 +0200

    Ajout dossier Section Header

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html',  // Ou les chemins vers tes fichiers HTML/JS
+    './*.css',
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
