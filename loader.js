2087f06913a6c1a33a2d8a14df3a7b13f318d1c9
AdnamDemaurisa59 <adnam.kouddemaurisa@outlook.fr>
2024-10-22 13:26:53 +0200

    Ajout du projet final Tea Station

---
+++ loader.js
@@ -0,0 +1,32 @@
+
+
+gsap.to(".dot" , {
+    y : -60,
+
+    stagger : {
+        each: 0.2,
+        repeat: -1,
+        yoyo: true,
+
+    }
+
+})
+
+
+gsap.to(".shadow" , {
+    y : 60,
+    stagger : {
+        each: 0.2,
+        repeat: -1,
+        yoyo: true,
+    },
+    opacity : 0.1,
+
+}) 
+
+
+// Simulation d'un faux temps de chargement de 3 secondes
+setTimeout(function() {
+    
+    window.location.href = "./page-accueil/pageaccueil-teastation.html";
+}, 3000); 
